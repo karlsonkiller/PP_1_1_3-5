62128bf335e8df014c921591bbd49720b1c017fb
karlsonkiller <sergss0213@yandex.ru>
2024-03-15 18:06:47 +0300

    Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,25 +1,23 @@
 package jm.task.core.jdbc;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 
 public class Main {
+
     public static void main(String[] args) {
-        UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
-        dao.createUsersTable();
+        UserDaoHibernateImpl hib = new UserDaoHibernateImpl();
 
-        dao.saveUser("Паша1", "Павлов1", (byte) 1);
-        dao.saveUser("Паша2", "Павлов2", (byte) 6);
-        dao.saveUser("Паша3", "Павлов3", (byte) 65);
-        dao.saveUser("Паша4", "Павлов4", (byte) 12);
+        hib.createUsersTable();
 
-        dao.getAllUsers();
+        hib.saveUser("Петя", "Петров", (byte) 15);
 
-        dao.cleanUsersTable();
+        hib.getAllUsers();
 
-        dao.dropUsersTable();
+        hib.removeUserById(1);
+
+        hib.saveUser("Петя", "Петров", (byte) 15);
+        hib.cleanUsersTable();
 
 
     }
+
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,32 +15,64 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE users " +
+                    "(id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(50), lastname VARCHAR(50), age INT(3))").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception ignore) {}
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE users").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception ignore) {}
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            User user = new User(name, lastName, age);
+            session.beginTransaction();
+            session.persist(user);
+            session.getTransaction().commit();
+        } catch (IllegalArgumentException ignored) {
+        }
     }
+
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            User user = session.load(User.class, id);
+            session.delete(user);
+            session.getTransaction().commit();
+        } catch (Exception ignore) {}
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            return session.createQuery("from User", User.class).getResultList();
+
+        } catch (HibernateException e) {
+            throw new IllegalArgumentException(e);
+
+        }
+
     }
+
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("DELETE FROM users");
+            session.getTransaction().commit();
+        } catch (Exception ignore) {}
     }
 }
